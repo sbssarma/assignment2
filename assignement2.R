37e174fcda516ce70ec9d204f363dda3f72641b6
sbssarma <sbsclicks@gmail.com>
2015-03-18 06:09:40 +0530

    Assignment2 on Lexical Scoping

---
+++ assignement2.R
@@ -0,0 +1,51 @@
+## Below are two functions that are used to create a special object that stores a matrix and 
+## cache's its inverse.
+
+## The first function, makeCacheMatrix creates a special "matrix", 
+## which is really a list containing a function to:
+## 1)set the value of the matrix (set_matrix)
+## 2)get the value of the matrix (get_matrix)
+## 3)set the value of the inverse (set_inverse)
+## 4)get the value of the inverse (get_inverse)
+
+makeCacheMatrix <- function(x = matrix()) {
+	inverse <- NULL 					## Initially assigning 'NULL' to inverse
+	set_matrix <- function(y) {			
+		x <<- y 					## Setting the matrix 'x'
+		inverse <<- NULL
+	}
+	get_matrix <- function() x 				## Returning matrix 'x'
+	set_inverse <- function(solve) inverse <<- solve 	## Cache the value of the inverse 
+	get_inverse <- function() inverse 			## Returning inverse
+	list(set_matrix = set_matrix, get_matrix = get_matrix,
+	     set_inverse = set_inverse,
+	     get_inverse = get_inverse)
+}
+
+
+## The following function calculates the inverse of the special "matrix" created with the above function.
+## However, it first checks to see if the inverse has already been calculated. 
+## If so, it gets the inverse from the cache and skips the computation. 
+## Otherwise, it calculates the inverse of the data and sets the value of the inverse in the cache via 
+## the 'set_inverse' function.
+
+cacheSolve <- function(x, ...) {				## Return a matrix that is the inverse of 'x'
+	inverse <- x$get_inverse()				## Getting inverse
+	if(!is.null(inverse)) {					## Checking for the presence of inverse
+		message("getting cached data")			## Displaying message
+		return(inverse)
+	}
+	data <- x$get_matrix()					## Getting Matrix
+	inverse <- solve(data, ...)				## Using solve() to compute inverse
+	x$set_inverse(inverse)					## To cache the inverse
+	inverse 						## Returning the inverse
+}
+
+SOlution to be solved in this way.
+Eg
+a <- makeCacheMatrix(matrix(c(1,2,12,13), nrow=2 ,ncol=2))
+#check the summary of assigned a
+summary(a)
+#Then solve it using cacheSolve function created
+cacheSolve(a)
+#proved.
